bff71b7df7b03bbefa04c1b957e1bed72ad171f3
Nikolaos Korasidis <renelvon@gmail.com>
2019-11-02 15:54:28 +0100

    Add documentation in the epistasis module

--- epystatic/epistasis.py
+++ epystatic/epistasis.py
@@ -1,3 +1,8 @@
+"""
+COMPUTE EPISTASIS
+=================
+"""
+
 import itertools
 
 import numpy as np
@@ -9,16 +14,26 @@ from epystatic import utils
 
 
 def compute_epistasis(w, w_err, species=5, interval=False):
+    """Compute all epistatic interactions, via circuits and interaction
+    coordinates among 'species' species. Return calculations in the form of
+    3 matrices: results, standard errors, calculation tag.
+
+    If 'interval' is False, then results and standard errors are returned;
+    otherwise, low and high limits of epistasis are returned (intervals).
+    """
+    # First compute all circuit interactions
     c_epi = gen_all_circuit_interactions(w, w_err, species, interval=interval)
     c_results, c_errors, c_tags = c_epi
     results, errors, tags = [c_results], [c_errors], [c_tags]
 
+    # Second, compute all significant orders of interaction coordinates
     u_epi = gen_all_coordinate_interactions(w, w_err, species, interval=interval)
     for u_results, u_errors, u_tags in u_epi:
         results.append(u_results)
         errors.append(u_errors)
         tags.append(u_tags)
 
+    # Finally, join results and return to user
     all_results = np.hstack(results)
     all_errors = np.hstack(errors)
     all_tags = tuple(itertools.chain.from_iterable(tags))
@@ -26,14 +41,21 @@ def compute_epistasis(w, w_err, species=5, interval=False):
 
 
 def gen_all_circuit_interactions(w, w_err, species, interval=False):
-    # Only for circuits of order 3
+    """Compute all circuit interactions and standard errors. Use interval
+    arithmetic if interval==True.
+
+    If 'interval' is True, then w is interpreted as the fitness vector
+    corresponding to the low end of the intervals and w_err as the fitness
+    vector corresponding to the high end of intervals. Instead of results and
+    errors, intervals of epistasis are computed.
+    """
     assert 2**species == len(w), 'Fitness vector has bad length'
     assert 2**species == len(w_err), 'Fitness error vector has bad length'
 
     # Generate all circuit interactions of order 3
     # Shape: (num_of_circtuits, 2**3)
     circuits_a2t = circuits.gen_circuits_3()
-    
+
     # Generate all possible setups (projections) of order 3
     # Shape: (num_of_projections, 2**3)
     wp, w_tags = slicing.generate_all_projections(w, 2, species, 3)
@@ -43,6 +65,8 @@ def gen_all_circuit_interactions(w, w_err, species, interval=False):
         w_low_p = w
         w_high_p = w_err
 
+        # Split circuit matrix into nonnegative and nonpositive part,
+        # and use elementary rules of interval arithmetic to calcualte results.
         pos_a2t, neg_a2t = split_pos_neg(circuits_a2t)
 
         results_low = pos_a2t.dot(w_low_p) + neg_a2t.dot(w_high_p)
@@ -51,24 +75,35 @@ def gen_all_circuit_interactions(w, w_err, species, interval=False):
         results = results_low
         errors = results_high
     else:
-        # Calculate all interactions via inner product
-        # Shape: (num_of_circuits, num_of_projections)
+        # Calculate all interactions via inner product of circuits and
+        # fitnesses (wp).
+        # Result shape: (num_of_circuits, num_of_projections)
         results = circuits_a2t.dot(wp.T)
 
-        # Calculate std error of all interactions
-        # Shape: (num_of_circuits, num_of_projections)
+        # Calculate std error of all interactions via inner product of the
+        # squares of coefficients (circuit matrix) and squares of fitness
+        # errors (wp_err).
+        # Result shape: (num_of_circuits, num_of_projections)
         errors = np.sqrt((circuits_a2t**2).dot((wp_err**2).T))
 
+    # Generate tags for all circuit interactions.
     all_c_tags = (
         gen_circuit_tag(i, w_tag)
         for i in range(len(circuits_a2t))
         for w_tag in w_tags
     )
-    
+
+    # Remove duplicate interactions from the results (according to the
+    # interaction formula computed; see 'circuits').
     return filter_duplicates(results, errors, all_c_tags)
 
 
 def filter_duplicates(results, errors, tags):
+    """Drop results and corresponding errors if they correspond to known
+    duplicate circuit calculations.
+
+    For more information, see 'circuits'.
+    """
     unique_tags, drop_indices = [], []
     for i, tag in enumerate(tags):
         if tag in circuits.DUPLICATES:
@@ -78,14 +113,27 @@ def filter_duplicates(results, errors, tags):
 
     unique_results = np.delete(results.flatten(), drop_indices)
     unique_errors = np.delete(errors.flatten(), drop_indices)
+
     return unique_results, unique_errors, unique_tags
 
 
 def gen_circuit_tag(i, w_tag):
+    """Generate a tag corresponding to a circuit interaction.
+
+    It is the concatenation of the circuit name along with the projection tag.
+    """
     return '{0:c}_{1:s}'.format(ord('a') + i, w_tag)
 
 
 def gen_all_coordinate_interactions(w, w_err, species, interval=False):
+    """Generate all non-singleton coordinate interactions and standard errors
+    of all significant orders 'order' among 'species' species.
+
+    If 'interval' is True, then w is interpreted as the fitness vector
+    corresponding to the low end of the intervals and w_err as the fitness
+    vector corresponding to the high end of intervals. Instead of results and
+    errors, intervals of epistasis are computed.
+    """
     assert 2**species == len(w), 'Bad fitness vector length'
     assert 2**species == len(w_err), 'Bad fitness std vector length'
 
@@ -94,22 +142,29 @@ def gen_all_coordinate_interactions(w, w_err, species, interval=False):
 
 
 def gen_coordinate_interactions_of_order(w, w_err, species, order, interval=False):
-    # Generate all non-singular coordinate interactions of given order
+    """Generate all non-singleton coordinate interactions and standard errors
+    of given 'order' among 'species' species.
+
+    If 'interval' is True, then w is interpreted as the fitness vector
+    corresponding to the low end of the intervals and w_err as the fitness
+    vector corresponding to the high end of intervals. Instead of results and
+    errors, intervals of epistasis are computed.
+    """
     # Shape: (2**order - order - 1, 2**order)
     f_mat = fourier.generate_fourier_matrix(order)
 
     # Generate all possible setups (projections) of given order
     # Shape: (num_of_projections, 2**order)
     wp, w_tags = slicing.generate_all_projections(w, 2, species, order)
-    
-    
     wp_err = slicing.generate_all_projections(w_err, 2, species, order, tagged=False)
 
     if interval:
         w_low_p = w
         w_high_p = w_err
 
-        pos_a2t, neg_a2t = split_pos_neg(circuits_a2t)
+        # Split circuit matrix into nonnegative and nonpositive part,
+        # and use elementary rules of interval arithmetic to calcualte results.
+        pos_a2t, neg_a2t = split_pos_neg(f_mat)
 
         results_low = pos_a2t.dot(w_low_p) + neg_a2t.dot(w_high_p)
         results_high = pos_a2t.dot(w_high_p) + neg_a2t.dot(w_low_p)
@@ -120,8 +175,7 @@ def gen_coordinate_interactions_of_order(w, w_err, species, order, interval=Fals
         # Calculate all interactions via inner product
         # Shape: (2**order - order - 1, num_of_projections)
         results = f_mat.dot(wp.T)
-        
-        
+
         # Calculate std error of all interactions
         # Shape: (2**order - order - 1, num_of_projections)
         errors = np.sqrt((f_mat**2).dot((wp_err**2).T))
@@ -132,12 +186,24 @@ def gen_coordinate_interactions_of_order(w, w_err, species, order, interval=Fals
         for w_tag in w_tags
         if i & (i - 1) # evaluates to 0 iff i == 0 or i == 2**j for j >= 0
     )
+
     return results.flatten(), errors.flatten(), u_tags
 
 
+#TODO: Write more clearly
+#TODO: Optimize
 def gen_coordinate_tag(formula, order, w_tag):
-    #TODO: Write more clearly
-    #TODO: Optimize
+    """Generate a tag corresponding to an interaction coordinate
+
+    The interpretation of the tag is as follows: Numbers appear in the
+    position of fixed (background) species, denoting the fixed value; letters
+    appear in the non-fixed species, in capital or small form, depending on the
+    index of the row of the Fourier matrix that is applied to the projection.
+
+    For example, the interaction u_101 applied to the w_A1BC0 projection will
+    result in the u_A1bC0 result tag, as A and C correspond to '1' in u_101,
+    whereas B corresponds to 0.
+    """
     aux = 1 << order
     desc = list(w_tag)
     for i, c in enumerate(desc):
@@ -223,5 +289,3 @@ if __name__ == '__main__':
     results, errors, tags = compute_epistasis(w, w_err, 5)
     for result, error, tag in zip(results, errors, tags):
        print(tag, result, error)
-
-
